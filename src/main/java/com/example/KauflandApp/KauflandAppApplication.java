2d7903029f87bb1c47acf35817223cec4712c3a7
Mozhnevskyi <rostislav.mozhnevskiy@gmail.com>
2026-07-18 11:50:29 +0200

    project initialization

---
+++ src/main/java/com/example/KauflandApp/KauflandAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.KauflandApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KauflandAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KauflandAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KauflandApp/KauflandAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KauflandApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KauflandAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
